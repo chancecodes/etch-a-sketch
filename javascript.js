48070fda9bc6f3362d252bb4eebb30ea1318197d
chancecodes <chanceycoding@gmail.com>
2022-11-21 16:07:49 -0800

    add shading capabilities to each square

--- javascript.js
+++ javascript.js
@@ -11,12 +11,14 @@ var gridLength = length.innerHTML;
 const sketchArea = document.querySelector('.sketchArea');
 var square;
 //initialize buttons
-const color = document.getElementById('color')
+const color = document.getElementById('color');
 color.classList.add("buttonOn");
-const eraser = document.getElementById ('eraser')
-const rainbow = document.getElementById('rainbow')
-const clear = document.getElementById('clear')
-addColor();
+var coloringMode = true;
+const eraser = document.getElementById ('eraser');
+const rainbow = document.getElementById('rainbow');
+const shade = document.getElementById('shade');
+
+
 
 // Update current slider value when you drag the slider handle and set to gridLength
 slider.oninput = function() {
@@ -38,13 +40,18 @@ pickColor.onchange = function() {
     penColor = userPick;
 };
 
-//select background color
-var bgColor = document.getElementById('bgColor');
+// //select background color
+// var pickBgColor = document.getElementById('bgColor');
+// var bgColor = pickBgColor.value;
 
-bgColor.onchange = function() {
-    bgColor = this.value;
-    sketchArea.style.backgroundColor = bgColor;
-};
+// pickBgColor.onchange = function() {
+//     bgColor = this.value;
+//     sketchArea.style.backgroundColor = bgColor;
+// };
+
+
+//create initial grid
+addColor();
 
 
 function createGrid (num) {
@@ -60,6 +67,8 @@ function createGrid (num) {
         for (let y=0; y < num; y++) {
         const square = document.createElement('div');
         square.classList.add ('gridSquare');
+        square.style.backgroundColor = "rgb(255,255,255)";
+        square.style.filter = "brightness(1)";
         column.appendChild(square);
         }
     }
@@ -79,38 +88,48 @@ function addColor() {
         window.addEventListener ('mouseup', stopColoring);
     };
 
-function coloring() {
-    if (rainbowMode === "on") {
+function coloring(e) {
+    if (rainbowMode) {
         randomColor()
     }
 
-    if (eraserMode === "on") {
-        penColor = ""
+    if (coloringMode) {
+        e.target.style.backgroundColor = penColor;
     }
 
-    this.style.backgroundColor = penColor;
+    if (eraserMode) {
+        penColor = "rgb(255,255,255)"
+        e.target.style.filter = "brightness(1)"
+    }
+
+    if (shadingMode) {
+        var getBrightness = e.target.style.filter
+        var brightnessValue = getBrightness.replace(/[brightness()]/g, "")
+        e.target.style.filter = `brightness(${brightnessValue - .1})`
+        console.log(e.target.style.filter);
+    }
+
+    // e.target.style.filter = "brightness(1)"
 
     square.forEach((box) => {
-        box.addEventListener ('mouseenter', dragColor);
+        box.addEventListener ('mouseenter', coloring);
     });
 };
 
 function stopColoring() {
     square.forEach((box) => {
-        box.removeEventListener ('mouseenter', dragColor);
+        box.removeEventListener ('mouseenter', coloring);
     });
 };
 
-function dragColor () {
-    if (rainbowMode === "on") {
-        randomColor()
-    }
-    if (eraserMode ==="on") {
-        penColor = ""
-    }
+//remove highlight from button if new button clicked
+function removeHighlight () {
+    const btn = document.querySelectorAll('.button')
+    btn.forEach(button => {
+        button.classList.remove('buttonOn')
+    })
+}
 
-    this.style.backgroundColor = penColor;
-};
 
 //RAINBOW MODE
 rainbow.addEventListener('click', activateRainbowMode)
@@ -124,43 +143,67 @@ function randomColor () {
 }
 
 function activateRainbowMode () {
-    rainbowMode = "on";
+    removeHighlight();
+    rainbowMode = true;
+    coloringMode = true;
     rainbow.classList.add("buttonOn");
 
-    colorMode = null;
-    color.classList.remove("buttonOn")
-    eraserMode = null;
-    eraser.classList.remove("buttonOn")
+    eraserMode = false;
+    shadingMode = false;
 }
 
 //Color Mode
 color.addEventListener ('click', activateColorMode);
-var colorMode;
 
 function activateColorMode() {
-    colorMode = "on"
+    removeHighlight();
+    coloringMode = true
     color.classList.add("buttonOn");
     penColor = userPick;
 
-    rainbowMode = null;
-    rainbow.classList.remove("buttonOn");
-    eraserMode = null;
-    eraser.classList.remove("buttonOn")
+    rainbowMode = false;
+    eraserMode = false;
+    shadingMode = false;
 }
+
+
+//Shading Mode
+shade.addEventListener ('click', activateShading);
+var shadingMode;
+
+function activateShading() {
+    removeHighlight();
+    shadingMode = true;
+    shade.classList.add("buttonOn")
+
+    rainbowMode = false
+    eraserMode = false
+    coloringMode = false
+}
+
 
 //Eraser
 eraser.addEventListener ('click', activateEraser)
 var eraserMode;
 
 function activateEraser() {
-    eraserMode = "on"
-    eraser.classList.add("buttonOn")
+    removeHighlight();
+    eraserMode = true;
+    coloringMode = true;
+    eraser.classList.add("buttonOn");
 
-    colorMode = null;
-    color.classList.remove("buttonOn")
-    rainbowMode = null;
-    rainbow.classList.remove("buttonOn");
+    rainbowMode = false;
+    shadingMode = false;
 }
 
 //clear board
+const clear = document.getElementById('clear')
 clear.addEventListener ('click', clearBoard);
+
+function clearBoard() {
+    square = document.querySelectorAll('.gridSquare')
+    square.forEach (box => {
+        box.style.backgroundColor = ""
+        box.style.filter = "brightness(1)"
+    })
+}
